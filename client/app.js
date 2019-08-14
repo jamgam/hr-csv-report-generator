5c59adf171d6480b4549bde0f9e7e445ac065f7d
Bryan Liang <bryanliang1995@gmail.com>
2019-08-13 18:49:15 -0700

    Allow the user to down the csv file

--- client/app.js
+++ client/app.js
@@ -1,5 +1,61 @@
-console.log('hello world');
 
-var myFunc = () =>{
-  console.log(1);
+$('.textSubmit').on('submit', function(e) {
+  e.preventDefault();
+
+  try {
+    JSON.parse($('.submitText').val());
+    $.ajax({
+      url: 'http://localhost:3000/text',
+      type: 'POST',
+      data: $('.submitText').val(),
+      contentType: 'application/json',
+      success: function(data) {
+        $('.csvText').val(data);
+        console.log('data', data);
+      }
+    });
+    console.log($('.submitText').val());
+  } catch (err) {
+    $('.message').html('ERROR: text must be in JSON format');
+  }
+
+});
+
+$('.fileSubmit').submit('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData();
+  formData.append('file', $('.file')[0].files[0] );
+
+  $.ajax({
+    url: 'http://localhost:3000/file',
+    type: 'POST',
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: function(data) {
+      if (Object.keys(data).length) {
+        $('.csvText').val(data);
+      } else {
+        $('.message').html('ERROR: file must be in JSON format');
+      }
+    }
+  });
+});
+
+$('.downloadBtn').on('click', function(e) {
+  let fileName = prompt('Save file as');
+  // $(this).attr('download', 'test' + '.csv');
+  // $(this).attr('href', 'data:text/csv;base64,' + btoa($('.csvText').val()));
+  save(fileName); 
+});
+
+var save = (fileName) => {
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/csv;base64,' + btoa($('.csvText').val()));
+  a.setAttribute('download', fileName + '.csv');
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 };
+
--- server.js
+++ server.js
@@ -1,30 +1,82 @@
 var express = require('express');
 var app = express();
-var morgan = require('morgan');
 var multer = require('multer');
 var upload = multer();
 var port = 3000;
+var bodyParser = require('body-parser');
 
-app.use(morgan('dev'));
+
+app.set('view engine', 'pug');
+app.use(require('morgan')('dev'));
 app.use(express.static('client'));
-// app.use(bodyParser.json());
+app.use(bodyParser.json({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res, next) => {
-  console.log('just /');
-  res.redirect('/index.html');
-
+  res.render('index', {message: 'Choose a file or input into textbox'});
 });
 
-app.get('/test', (req, res, next) => {
-  console.log('works');
-  res.end();
+app.post('/text', (req, res, next) => {
+  var result;
+  console.log('csv: ', convertToCSV(req.body));
+  try {
+    res.send(convertToCSV(req.body));
+  } catch (err) {
+    res.send(err);
+  }
+  // res.send(convertToCSV(req.body));
 });
 
-app.post('/csv', upload.none(), (req, res, next) => {
-  console.log('body', req.body);
-  res.redirect('/');
+app.post('/file', upload.single('file'), (req, res) => {
+  var obj;
+  try {
+    obj = JSON.parse(req.file.buffer.toString());
+    res.send(convertToCSV(obj));
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
+
+const oneLineOfCSV = (obj) => {
+  var testStr = [];
+  for (let key of Object.keys(obj)) {
+    if (key !== 'children') {
+      testStr.push(obj[key]);
+    }
+  }
+  testStr = testStr.join(',');
+  testStr += '\n';
+  return testStr;
+};
+
+const getKeys = (obj) => {
+  var testStr = [];
+  for (let key of Object.keys(obj)) {
+    if (key !== 'children') {
+      testStr.push(key);
+    }
+  }
+  testStr = testStr.join(',');
+  testStr += '\n';
+  return testStr;
+};
+
+const convertToCSV = (obj) => {
+  var result = '';
+  result += getKeys(obj);
+  var recurse = (obj) => {
+    result += oneLineOfCSV(obj);
+    
+    if (obj.children) {
+      for (let child of obj.children) {
+        recurse(child);
+      }
+    }
+  };
+  recurse(obj);
+  return result;
+};
